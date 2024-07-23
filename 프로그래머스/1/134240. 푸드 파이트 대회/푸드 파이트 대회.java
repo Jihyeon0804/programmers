833b153037e6d4e885f07cafdf383f65c73e2e87
Jihyeon0804 <80299604+Jihyeon0804@users.noreply.github.com>
2024-07-23 21:48:49 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 12.66 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        for (int i = 1; i < food.length; i++) {
+            answer += Integer.toString(i).repeat(food[i] / 2);
+        }
+        
+        StringBuffer sb = new StringBuffer(answer);
+        answer = answer + "0" + sb.reverse().toString();
+        return answer;
+    }
+}
