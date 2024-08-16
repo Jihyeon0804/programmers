c563af414f31c28fef3c319f06fed121bb6e371d
Jihyeon0804 <80299604+Jihyeon0804@users.noreply.github.com>
2024-08-16 12:38:48 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        System.out.println() ;
-        return Math.sqrt(n) % (int) Math.sqrt(n) == 0 ? 1 : 2;
+        
+        return n %  Math.sqrt(n) == 0 ? 1 : 2;
     }
 }
