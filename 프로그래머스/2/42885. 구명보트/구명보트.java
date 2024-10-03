28312f1f2bdf5aabdb7cedf5e3ac0974b794e9a9
Jihyeon0804 <80299604+Jihyeon0804@users.noreply.github.com>
2024-10-03 21:54:11 +0900

    [level 2] Title: 구명보트, Time: 13.66 ms, Memory: 56.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int start = 0;
+        int end = people.length - 1;
+        
+        while (start <= end) {
+            answer++;
+            if (people[start] + people[end] <= limit) {
+                start++;
+            }
+            end--;
+        }
+        return answer;
+    }
+}
